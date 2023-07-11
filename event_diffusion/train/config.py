08850251eab2ed736754a83d91924002e2fc6b01
Michael Doyle <mdoyle@mitre.org>
2023-07-11 14:27:27 -0600

    Update config

--- event_diffusion/train/config.py
+++ event_diffusion/train/config.py
@@ -4,15 +4,16 @@ from dataclasses import dataclass
 @dataclass
 class TrainingConfig:
     image_size = 128  # the generated image resolution
-    train_batch_size = 16
+    train_batch_size = 32
     eval_batch_size = 16  # how many images to sample during evaluation
-    num_epochs = 50
+    num_eval_inference_steps = 10
+    num_epochs = 100
     gradient_accumulation_steps = 1
     learning_rate = 1e-4
     lr_warmup_steps = 500
     save_image_epochs = 1
-    save_model_epochs = 30
-    mixed_precision = "fp16"  # `no` for float32, `fp16` for automatic mixed precision
+    save_model_epochs = 25
+    mixed_precision = "no"  # `no` for float32, `fp16` for automatic mixed precision
     output_dir = "conditional-event-diffusion"  # where to save the trained model
 
     push_to_hub = False  # whether to upload the saved model to the HF Hub
